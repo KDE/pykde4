7e5b6a2a1c7c80083b682cda377dbcac17498188
Benjamin Reed <ranger@befunk.com>
2008-01-24 14:48:33 +0000

    builds on mac; smoke and krosspython support
    svn path=/trunk/KDE/kdebindings/python/pykde4/; revision=765702

--- cmake/modules/FindPythonLibrary.cmake
+++ cmake/modules/FindPythonLibrary.cmake
@@ -22,6 +22,8 @@
 #
 # PYTHON_LIBRARY, PYTHON_LIBRARIES- Location of the Python library.
 
+INCLUDE(CMakeFindFrameworks)
+
 IF(PYTHON_LIBRARY)
    # Already in cache, be silent
    SET(PYTHONLIBRARY_FOUND TRUE)
@@ -38,11 +40,29 @@ ELSE(PYTHON_LIBRARY)
     STRING(REGEX REPLACE ".*\npy_inc_dir:([^\n]+).*$" "\\1" PYTHON_INCLUDE_PATH ${python_config})
     STRING(REGEX REPLACE ".*\nsite_packages_dir:([^\n]+).*$" "\\1" PYTHON_SITE_PACKAGES_DIR ${python_config})
     FIND_LIBRARY(PYTHON_LIBRARY NAMES python${PYTHON_SHORT_VERSION} PATHS ${PYTHON_PREFIX}/lib NO_DEFAULT_PATH)
-    SET(PYTHON_LIBRARIES ${PYTHON_LIBRARY})
     SET(PYTHONLIBRARY_FOUND TRUE)
   ENDIF(python_config)
 
+  # adapted from cmake's builtin FindPythonLibs
+  if(APPLE)
+    CMAKE_FIND_FRAMEWORKS(Python)
+    SET(PYTHON_FRAMEWORK_INCLUDES)
+    IF(Python_FRAMEWORKS)
+      # If a framework has been selected for the include path,
+      # make sure "-framework" is used to link it.
+      IF("${PYTHON_INCLUDE_PATH}" MATCHES "Python\\.framework")
+        SET(PYTHON_LIBRARY "")
+        SET(PYTHON_DEBUG_LIBRARY "")
+      ENDIF("${PYTHON_INCLUDE_PATH}" MATCHES "Python\\.framework")
+      IF(NOT PYTHON_LIBRARY)
+        SET (PYTHON_LIBRARY "-framework Python" CACHE FILEPATH "Python Framework" FORCE)
+      ENDIF(NOT PYTHON_LIBRARY)
+      SET(PYTHONLIBRARY_FOUND TRUE)
+    ENDIF(Python_FRAMEWORKS)
+  ENDIF(APPLE)
+
   IF(PYTHONLIBRARY_FOUND)
+    SET(PYTHON_LIBRARIES ${PYTHON_LIBRARY})
     IF(NOT PYTHONLIBRARY_FIND_QUIETLY)
       MESSAGE(STATUS "Found Python executable: ${PYTHON_EXECUTABLE}")
       MESSAGE(STATUS "Found Python version: ${PYTHON_LONG_VERSION}")

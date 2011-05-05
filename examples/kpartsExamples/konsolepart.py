40dab8330c0e0e0d9d79eccc4885533c0b15e402
Shaheed Haque <srhaque@theiet.org>
2011-05-05 20:52:33 +0100

    Add support for konsolepart to PyKDE4 binding
    In order to access the KTerminalInterface and KTerminalInterfaceV2
    exposed by konsolepart, it is necessary to make use of qobject_cast.
    This is not something that seems to be possible from PyQt4 and so some
    C++ help is needed. This binding provides that C++ helper code, and
    wrappers to match.
    
    A sample program is also provided.
    REVIEW: 101269
    DIGEST: PyKDE4 get support for konsolepart.

---
+++ examples/kpartsExamples/konsolepart.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python                                                                   
+#
+# Copyright (c) 2011 Shaheed Haque (srhaque@theiet.org)
+#
+# This file is part of PyKDE4.
+#
+# PyKDE4 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
+#
+# PyKDE4 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http:www.gnu.org/licenses/>.
+# 
+
+# KTerminal is a KPart which implements KTerminalInterfaceV2. This sample
+# illustrates how to use KTerminal to create an instance of konsolepart,
+# and send it some things to do.
+ 
+import sys
+# Uncomment this if you are doing development, and installing the 
+# work-in-progress here.
+sys.path.insert(1,'/usr/local/lib/python2.7/site-packages')
+from PyKDE4.kterminal import KTerminal
+from PyQt4.QtCore import QStringList
+from PyKDE4.kdecore import ki18n, KAboutData, KCmdLineArgs, KPluginLoader
+from PyKDE4.kparts import KParts
+from PyKDE4.kdeui import *
+
+class MainWindow (KMainWindow):
+    def __init__ (self):
+        KMainWindow.__init__(self)
+
+        self.resize(640, 480)
+        factory = KPluginLoader('libkonsolepart').factory()
+        part = factory.create(self, "KonsolePart")
+        self.setCentralWidget(part.widget())
+        terminal = KTerminal(part).terminalInterfaceV2()
+        terminal.startProgram("/bin/bash", QStringList())
+        terminal.sendInput("ls -l\n");
+
+if __name__ == '__main__':
+
+    appName     = "konsolepart_example"
+    catalog     = ""
+    programName = ki18n("Konsole Part Example")
+    version     = "1.0"
+    description = ki18n("Example loading a Konsole Part")
+    license     = KAboutData.License_GPL
+    copyright   = ki18n("(c) Shaheed Haque")
+    text        = ki18n("none")
+    homePage    = "http://techbase.kde.org/Development/Languages/Python/PyKDE_KTerminal_Tutorial"
+    bugEmail    = "srhaque@theiet.org"
+    
+    aboutData   = KAboutData(appName, catalog, programName, version, 
+                             description, license, copyright, text, homePage,
+                             bugEmail)
+            
+    KCmdLineArgs.init(sys.argv, aboutData)
+        
+    app = KApplication()
+    mainWindow = MainWindow()
+    mainWindow.show()
+    app.exec_()
